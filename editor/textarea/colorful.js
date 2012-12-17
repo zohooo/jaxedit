7210f27b3f32f7a9783a6cbaba53a491983e8648
zohooo <zohooo@yeah.net>
2012-12-18 00:03:42 +0800

    fix change event handler for codemirror

--- editor/textarea/colorful.js
+++ editor/textarea/colorful.js
@@ -2,15 +2,23 @@
 // copyright (c) 2012 JaxEdit project
 
 jaxedit.cmChange = function(editor, change) {
-  console.log(change.from, change.to, change.text);
+  console.log(change.from, change.to, change.text, change.next);
+  var data = this.textdata;
+  data.oldtextvalue = data.newtextvalue;
+  data.oldtextsize  = data.newtextsize;
   var textvalue = editor.getValue(),
       textsize = textvalue.length,
+      instext = change.text.join('\n'),
       delstart = editor.indexFromPos(change.from),
-      delend = editor.indexFromPos(change.to),
-      deltext = editor.getRange(change.from, change.to),
-      instext = change.text.join('\n');
+      delend = delstart + data.oldtextsize + instext.length - textsize,
+      deltext = data.oldtextvalue.substring(delstart, delend);
   console.log(delstart, delend, deltext, instext, textsize);
+  data.newtextvalue = textvalue;
+  data.newtextsize = textsize;
   typejax.updater.puttask(delstart, delend, deltext, instext, textsize, jaxedit.childs.showarea);
+  if (change.next) {
+    arguments.callee.call(this, editor, change.next);
+  }
 };
 
 jaxedit.addCodeMirror = function() {
