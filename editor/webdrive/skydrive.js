01842308cc6b4e173a586d67b9b8b253b369d254
zohooo <zohooo@yeah.net>
2013-01-09 11:25:43 +0800

    update license infomation

--- editor/webdrive/skydrive.js
+++ editor/webdrive/skydrive.js
@@ -1,5 +1,12 @@
 
-// Copyright (c) 2012 JaxEdit Project
+/* JaxEdit: online LaTeX editor with live preview
+ * Copyright (c) 2011-2013 JaxEdit project
+ * License: GNU General Public License, Version 3
+ *
+ * Website: http://jaxedit.com
+ * Source:  https://github.com/zohooo/jaxedit
+ * Release: http://code.google.com/p/jaxedit/
+ */
 
 var skydrive = {
   finside: [],
